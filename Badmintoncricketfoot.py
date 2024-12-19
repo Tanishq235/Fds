8627973fe64f59ed1f31cdd544563ff17085f623
Tanishq235 <tanishqkadam23@gmail.com>
2024-12-19 16:08:54 +0530

    Update Badmintoncricketfoot.py

--- Badmintoncricketfoot.py
+++ Badmintoncricketfoot.py
@@ -3,17 +3,20 @@ n = int(input("Enter the number of students who play cricket := "))
 for i in range(n):
     k = int(input("Enter the roll no of student who plays cricket := "))
     cricket.append(k)
+
 badminton = []
 n1 = int(input("Enter the number of students who play badminton := "))
 for i in range(n1):
     k1 = int(input("Enter the roll no of student who plays badminton := "))
     badminton.append(k1)
+
 football = []
 n2 = int(input("Enter the number of students who play football := "))
 for i in range(n2):
     k2 = int(input("Enter the roll no of student who plays football := "))
     football.append(k2)
-total_stud = badminton + cricket + football 
+
+total_stud = cricket + badminton + football
 
 cb = []
 for i in cricket:
@@ -23,15 +26,16 @@ for i in badminton:
         cb.append(i)
 print("Students roll no who play badminton and cricket =", cb)
 
-
 bcb = []
 for i in cb:
     if (i in cricket) and (i in badminton):
         bcb.append(i)
 print("Roll no of students who play both cricket and badminton =", bcb)
 
+all_students = set(cricket + badminton + football)
+
 ncb = []
-for i in range(1, max(total_stud) + 1):  
+for i in all_students:  
     if i not in cricket and i not in badminton:
         ncb.append(i)
 print("Roll no of students who neither play cricket nor badminton =", ncb)
