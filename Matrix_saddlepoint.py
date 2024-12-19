0302ff141c9f3dce5848db9e5dc9f9f909f982a9
Tanishq235 <tanishqkadam23@gmail.com>
2024-12-19 18:51:57 +0530

    Update Matrix_saddlepoint.py

--- Matrix_saddlepoint.py
+++ Matrix_saddlepoint.py
@@ -1,28 +1,35 @@
-row=int(input("Enter the no of elements in row"))
-column=int(input("Enter the no of elements in column"))
-A=[]
+row = int(input("Enter the number of elements in row: "))
+column = int(input("Enter the number of elements in column: "))
+A = []
 for i in range(row):
-    p=[]
+    p = []
     for j in range(column):
-         a=int(input("Enter the no of element in position " +str(i)+ ","+str(j)))
-         p.append(a)
+        a = int(input(f"Enter the number of element in position ({i},{j}): "))
+        p.append(a)
     A.append(p)
-print("Matrix is :=",A)
+print("Matrix is:")
 for i in A:
     print(i)
-k=[]
+k = []
 for i in A:
-    a1=min(i)
+    a1 = min(i)
     k.append(a1)
-b=min(k)
+b = min(k)
+found_saddle_point = False
 for i in A:
     if b in i:
-        c=i.index(b)
+        c = i.index(b)
+        found_saddle_point = True
+        break
+if found_saddle_point:
+    Q = []
+    for i in A:
+        l = i[c]
+        Q.append(l)
+    c1 = max(Q)
+    if b == c1:
+        print("Saddle point is:", c1)
     else:
-        pass
-Q=[]
-for i in A:
-    l=i[c]
-    Q.append(l)
-c1=max(Q)
-print("SADDLE POINT IS :=",c1)
+        print("Saddle point is not present")
+else:
+    print("Saddle point is not present")
